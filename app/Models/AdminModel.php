903a23508619944ed9ebe8579722beb8fa4d040c
marvellousshoyoye@gmail.com <marvellousshoyoye@gmail.com>
2025-02-26 14:17:38 +0100

    update

--- app/Models/AdminModel.php
+++ app/Models/AdminModel.php
@@ -7,6 +7,6 @@ class AdminModel extends Model
 {
     protected $table = 'admins';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['name', 'email', 'password'];
+    protected $allowedFields = ['name', 'email', 'password',];
     protected $useTimestamps = true;
 }
